fd998820115f3a60e94df70cc7a4d80869b840ec
Nevar <psi.neamf@gmail.com>
2011-10-17 22:17:00 +0200

    Updated to check local then remote for package.

--- pacshare/xferclient.py
+++ pacshare/xferclient.py
@@ -1,6 +1,15 @@
 import argparse
 import logging
+import os
 import sys
+
+from urllib.request import FancyURLopener
+
+from pacshare.progressbar import ProgressBar
+
+class MyURLOpener(FancyURLopener):
+    def http_error_404(self, url, fp, errcode, errmsg, headers):
+        print(url, errcode, errmsg)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -10,3 +19,21 @@ def main():
     
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+
+    org_url = args.url
+    package = os.path.basename(org_url)
+    save_location = args.filename
+
+    pb = ProgressBar(filename=package)
+
+
+    url = 'http://localhost:16661/%s' % package
+    try:
+        logging.debug('Downloading {} to {}'.format(url, args.filename))
+        MyURLOpener().retrieve(url, save_location, pb.update)
+    except:
+        try:
+            logging.debug('Downloading {} to {}'.format(org_url, args.filename))
+            MyURLOpener().retrieve(org_url, save_location, pb.update)
+        except:
+            pass
