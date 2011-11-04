c4938b620153c957ea068a58c8ba3a97a2737edb
Nevar <psi.neamf@gmail.com>
2011-11-04 12:41:26 +0200

    Work on server serving from different directory structures.

--- pacshare/server.py
+++ pacshare/server.py
@@ -12,12 +12,28 @@ class PacShareHTTPRequestHandler(BaseHTTPRequestHandler):
             f.close()
 
     def get_file(self):
-        package_cache = '/var/cache/pacman/pkg'
-        package = self.path.strip('/')
+        #repo,os,arch
+
+        cache_root = '/var/cache/pacman/pkg'
+
+        cache_structure = 'flat'
+        #cache_structure = 'Arch Mirror'
+
+        path = self.path.strip('/')
+
+        if cache_structure == 'Arch Mirror':
+            pass
+        elif cache_structure == 'flat':
+            path = os.path.basename(path)
+            #if path.endswith('.db'):
+            #    cache_root = '/var/lib/pacman/sync'
+
+        package_location = os.path.join(cache_root, path)
+
         f = None
 
         try:
-            f = open(os.path.join(package_cache, package), 'rb')
+            f = open(package_location, 'rb')
         except IOError:
             self.send_error(404, "File not found")
             return None
