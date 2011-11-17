c29ca1ec29716267740567764dc1a7c78e23d805
Gary van der Merwe <garyvdm@gmail.com>
2011-11-17 10:34:23 +0200

    Refactor xferclient and improve progressbar/logging interaction.

--- pacshare/xferclient.py
+++ pacshare/xferclient.py
@@ -9,61 +9,9 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.parse import urlparse
 
-from progressbar import Bar, ETA, FileTransferSpeed, \
-                        Percentage, ProgressBar
+import progressbar
 
 from pacshare import avahi
-
-
-def get_progressbar(filename, maxval):
-    widgets = ['{:30}'.format(filename), ETA(), '  ',
-               FileTransferSpeed(), ' ', Bar(), ' ',
-               Percentage()]
-    pbar = ProgressBar(widgets=widgets, maxval=maxval)
-    return pbar
-
-def retrieve_data(open_url, save_location):
-    total_size = int(open_url.getheader('Content-Length') or '0')
-    pbar = get_progressbar(os.path.basename(open_url.geturl()), total_size)
-    if total_size:
-        pbar.start()
-    else:
-        logging.info('Downloading file ... (no progess available)')
-    block_size = 4096
-
-    try:
-        with open(save_location, 'wb') as filename:
-            while True:
-                chunk = open_url.read(block_size)
-                if not chunk:
-                    break
-                if pbar.start_time is not None:
-                    pbar.update(pbar.currval + len(chunk))
-                filename.write(chunk)
-    finally:
-        if pbar.start_time is not None:
-            pbar.finish()
-
-
-package_re = re.compile('.*/(.*\.pkg\.tar\.xz)$')
-def gen_urls(url):
-    up = urlparse(url)
-    package_name_match = package_re.search(up.path)
-    if package_name_match:
-        logging.debug('Package detected. Atempting to fetch from peer pacshares.')
-        services = avahi.service_browser_get_cache('_pacshare._tcp')
-        # remove the duplicates due to multiple interfaces/protocals.
-        services = list(dict((service.name, service) for service in services).values())
-        random.shuffle(services)
-        for service in services:
-            if not (service.flags & avahi.LookupResultFlags.LOCAL):
-                resolved_service = avahi.resolve_service(service.name, service.type, service.domain)
-                
-                logging.debug('Atempting fetch from {}'.format(resolved_service.host_name))
-                yield 'http://{}:{}/{}'.format(
-                    resolved_service.address, resolved_service.port,
-                    package_name_match.group(1))
-    yield url
 
 def main():
     parser = argparse.ArgumentParser()
@@ -72,23 +20,130 @@ def main():
     parser.add_argument('filename', help='Filename to save as')
     
     args = parser.parse_args()
-    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+    global log_handler
+    log_handler = StreamHandlerProgressBarClear(sys.stdout)
+    log_handler.setFormatter(logging.Formatter('%(levelname)s:%(message)s'))
+    root_log = logging.getLogger()
+    root_log.addHandler(log_handler)
+    root_log.setLevel(logging.DEBUG if args.verbose else logging.INFO)
 
-    
     try:
-        for url in gen_urls(args.url):
-            try:
-                logging.debug('Downloading {} to {}'.format(url, args.filename))
-                open_url = urlopen(url)
-            except HTTPError as e:
-                logging.debug('Error Downloading {} : {}'.format(url, e))
-            except Exception:
-                logging.exception('Downloading {}'.format(url))
-            else:
-                retrieve_data(open_url, args.filename)
-                return 0
-        logging.error('Could not retrieve file from any url.')
-        return 1
+        fetch_success = fetch_url(args.url, args.filename)
+        if fetch_success:
+            return 0
+        else:
+            return 2
     except KeyboardInterrupt as e:
         logging.debug('User pressed CTRL-C')
         return 1
+    except:
+        logging.exception('')
+        return 2
+
+class StreamHandlerProgressBarClear(logging.StreamHandler):
+    
+    def emit(self, record):
+        try:
+            pbar = getattr(self, 'pbar', None)
+            if pbar:
+                pbar.fd.write((' ' * pbar.term_width) + '\r')
+        except:
+            self.handleError(record)
+        super().emit(record)
+        try:
+            if pbar:
+                pbar.update()
+        except:
+            self.handleError(record)
+
+def get_pacshare_peers():
+    logging.debug('Getting pacshare peers from avahi.')
+    services = avahi.service_browser_get_cache('_pacshare._tcp')
+    services = [service for service in services
+                if not (service.flags & avahi.LookupResultFlags.LOCAL)]
+    if services:
+        logging.debug('Peers found: %s.', [service.name for service in services])
+    else:
+        logging.debug('No peers found.')
+    for service in services:
+        yield avahi.resolve_service(
+            service.name, service.type, service.domain)
+
+
+package_re = re.compile('(.*)\.pkg\.tar\.xz$')
+def fetch_url(url, filename):
+    status_widget = ProgressBarStatusWidget()
+    pbar = progressbar.ProgressBar(widgets=[
+        status_widget, '  ',
+        progressbar.ETA(), '  ', progressbar.FileTransferSpeed(), ' ',
+        progressbar.Bar(), ' ',
+        progressbar.Percentage()], maxval=100)
+    progressbar.ProgressBar._need_update = lambda self: True
+    
+    log_handler.pbar = pbar
+    try:
+        status_widget.format = '{0.resource_name} {0.server_from}'
+        status_widget.resource_name = ''
+        status_widget.server_from = ''
+        
+        def _fetch_url(url, error_level=logging.ERROR):
+            try:
+                logging.debug('Downloading {} to {}'.format(url, filename))
+                pbar.update()
+                open_url = urlopen(url)
+                total_size = int(open_url.getheader('Content-Length') or '0')
+                if total_size:
+                    pbar.maxval = total_size
+                
+                block_size = 1024        
+                with open(filename, 'wb') as file:
+                    while True:
+                        chunk = open_url.read(block_size)
+                        if not chunk:
+                            break
+                        file.write(chunk)
+                        pbar.update(pbar.currval + len(chunk))
+                logging.debug('Download successfull.')
+                pbar.finish()
+                return True
+            except HTTPError as e:
+                logging.log(error_level, 'Downloading {} : {}'.format(url, e))
+                return False
+            except Exception:
+                logging.exception('Downloading {}'.format(url))
+                return False
+        
+        up = urlparse(url)
+        path = up.path.split('/')[1:]
+        status_widget.resource_name = path[-1]
+        
+        pbar.start()
+        
+        package_name_match = package_re.match(path[-1])
+        if package_name_match:
+            logging.debug('Package detected.')
+            status_widget.resource_name = package_name_match.group(1)
+            status_widget.server_from = 'Getting pacshare peers.'
+            pbar.update()
+            for peer in get_pacshare_peers():
+                status_widget.server_from = 'from {0.host_name}'.format(peer)
+                fetch_success = _fetch_url(
+                    'http://{}:{}/{}'.format(peer.address, peer.port,
+                                             package_name_match.group(0)),
+                    logging.DEBUG)
+                if fetch_success:
+                    return True
+        
+        status_widget.server_from = 'from {0.netloc}'.format(up)
+        return _fetch_url(url)
+    except:
+        pbar.fd.write('\n')
+        raise
+    finally:
+        log_handler.pbar = None
+
+
+class ProgressBarStatusWidget(progressbar.Widget):
+    def update(self, pbar):
+        return self.format.format(self, pbar)
+
